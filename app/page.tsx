7d5b90bd6db9f26dfc5419f4537c225c74b0bd42
NazmusSakib2036 <nazmuss024@gmail.com>
2026-09-17 21:51:57 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Img from 'next/image';
+
+export const page = () => {
+  return (
+    <div className='flex flex-col items-center justify-center p-10'>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam ipsa numquam quod. Vel nam voluptate necessitatibus molestias laborum, pariatur temporibus provident! Necessitatibus quas deleniti corrupti, pariatur blanditiis sed magni quos modi nostrum itaque, eligendi in cumque laudantium quo animi, dolore praesentium similique neque excepturi. Magnam, consequatur eos illo distinctio culpa suscipit nam commodi molestiae vitae repellendus earum dolores excepturi dolor architecto temporibus dolorum eligendi explicabo quod sed. Vero quae fugiat in voluptate omnis iure dolorem aliquid, assumenda ipsum, quidem voluptas. Aliquam sequi voluptatum facere, veniam incidunt in doloremque eius doloribus autem. Id asperiores libero voluptates impedit sit quaerat, rerum perferendis accusamus accusantium suscipit. Necessitatibus alias neque beatae nesciunt aliquam et atque. Voluptatibus itaque, labore non quibusdam aut inventore nesciunt cumque voluptas at soluta, natus consequuntur recusandae blanditiis quia saepe culpa vitae nostrum omnis adipisci. Enim architecto laudantium alias temporibus ipsum labore sunt tempore deserunt? Ullam veniam in culpa nam dolores reprehenderit exercitationem cum sit magnam amet blanditiis dolore molestias et esse adipisci officiis assumenda facere, eligendi minus fuga, eos ratione odio explicabo? Nulla fugit quis saepe ipsa maxime itaque ad voluptates omnis! Maiores quasi quos excepturi. Corrupti, libero pariatur natus repellendus alias velit, culpa consequuntur, rem nostrum et fugit omnis.</p>
+
+    <a href="https://nazmussakibpro.vercel.app" target="_blank" rel="noopener noreferrer">
+      <Img src="/favicon.ico" alt="Description of the image" width={100} height={100} />
+    </a>
+
+    </div>
+  )
+}
+
+
+export default page;
